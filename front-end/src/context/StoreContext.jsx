400aee904f2f066b24c8bb2f92133fd32e7012c7
mohamed <medo92salah@gmail.com>
2024-05-10 16:38:19 +0300

    second commit added cart and place order pages

--- front-end/src/context/StoreContext.jsx
+++ front-end/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect } from "react";
+import { createContext, useContext, useEffect } from "react";
 import { booklist } from "../assets/assets";
 import { useState } from "react";
 
@@ -9,6 +9,7 @@ const StoreContextProvider =(props)=>{
 
     const [cartItems,setCartItems]=useState({});
 
+    // const {getTotalCartAmount}=useContext(StoreContext);
     const addToCart = (itemId) =>{
         if(!cartItems[itemId]){
             setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -22,16 +23,26 @@ const StoreContextProvider =(props)=>{
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
    }
 
-   useEffect(()=>{
-    console.log(cartItems)
-   },[cartItems])
+//    useEffect(()=>{
+//     console.log(cartItems)
+//    },[cartItems])
+
+const getTotalCartAmount=()=>{
+    let totalAmount=0;
+    for(const item in cartItems){
+        let itemInfo=booklist.find((product)=>product._id===item);
+        totalAmount+=itemInfo.price*cartItems[item];
+    }
+    return totalAmount;
+}
 
     const ContextValue={
         booklist,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
 
 
     }
