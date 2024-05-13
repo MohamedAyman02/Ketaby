dc7713cecdd4e0fdf75b6d9b2a99f620ca589f20
NoNo-T <nourantarek710@gmail.com>
2024-05-13 11:40:41 +0300

    user model

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { loginUser,registerUser } from "../controllers/usercontroller.js"
-
+//userroute
 const userRouter = express.Router()
 
 userRouter.post("/Register",registerUser)
